d6d57394cec4b0240ee5ad0966930b3b08f13704
Saurabh <saurabhkr0927@gmail.com>
2025-04-12 12:05:32 +0530

    updated the color mode

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,8 +4,6 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
-import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
 
 const Experience = () => {
